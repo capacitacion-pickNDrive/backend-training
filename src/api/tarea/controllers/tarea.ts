b1bf297cb1dbe29276f0d863047f443075b19523
emmabeni27 <larisabeni@mail.com>
2025-08-16 18:58:32 -0300

    cambios menores

---
+++ src/api/tarea/controllers/tarea.ts
@@ -0,0 +1,7 @@
+/**
+ * tarea controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::tarea.tarea');
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,41 @@
-// import type { Core } from '@strapi/strapi';
+// src/index.ts
 
 export default {
-  /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
-  register(/* { strapi }: { strapi: Core.Strapi } */) {},
+  register() {},
 
-  /**
-   * An asynchronous bootstrap function that runs before
-   * your application gets started.
-   *
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
-   */
-  bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
+  bootstrap: async ({ strapi }) => {
+    const existingTareas = await strapi.db.query('api::tarea.tarea').findMany();
+
+    if (existingTareas.length === 0) {
+      const categoria = await strapi.db.query('api::categoria.categoria').create({
+        data: {
+          nombre: 'Estudio',
+          color: '#3498db',
+        },
+      });
+
+      await strapi.db.query('api::tarea.tarea').create({
+        data: {
+          titulo: 'Repasar punteros en C',
+          descripcion: 'Usar *(p+i) y validar NULL',
+          categoria: categoria.id,
+        },
+      });
+
+      await strapi.db.query('api::tarea.tarea').create({
+        data: {
+          titulo: 'Configurar roles en Strapi',
+          descripcion: 'Limitar acceso a tareas solo a usuarios autenticados',
+          categoria: categoria.id,
+        },
+      });
+
+      console.log('🌱 Tareas de ejemplo creadas');
+    }
+  },
 };
+// con una buena restriccion no se duplican
+//lo dejo en el codigo, si lo hiciera en la pagina se pierde
+//strapi es para la interfaz web mientras que el ide para la logica
+
+//con npx busca el binario de strapi. Si no lo buscaba en el path y no estaba.
