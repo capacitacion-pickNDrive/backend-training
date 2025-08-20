78c62c9bc1e825edfafb25dd9c0f7deea7e7efff
bang_santiago <sbang@mail.austral.edu.ar>
2025-08-20 17:12:45 -0300

    Backend training

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ export default {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/* { strapi }: { strapi: Core.Strapi } */) {},
+  register(/* { strapi }: { strapi: Core.Strapi } */) { },
 
   /**
    * An asynchronous bootstrap function that runs before
@@ -16,5 +16,97 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
+  async bootstrap({ strapi }) {
+    // Check if 'Front-end' category already exists
+    const alreadySeeded = await strapi.db.query('api::category.category').findOne({
+      where: {
+        name: 'Front-end'
+      }
+    });
+
+    if (alreadySeeded == null) {
+
+      try {
+
+        /* 
+          Categories
+        
+          Colours:
+          * Red: #FF372F
+          * Orange: #FF9300
+          * Yellow: #FFFF00
+          * Green: #6AEB49
+          * Light-blue: #6AC7FF
+          * Blue: #1581FF
+          * Pink: #FF81FF
+        */
+
+        const categoryFront = await strapi.db.query('api::category.category').create({
+          data: {
+            name: 'Front-end',
+            color: '#6AC7FF'
+          }
+        });
+
+        const categoryBack = await strapi.db.query('api::category.category').create({
+          data: {
+            name: 'Back-end',
+            color: '#FF9300'
+          }
+        });
+
+        const categoryOther = await strapi.db.query('api::category.category').create({
+          data: {
+            name: 'Other',
+            color: '#6AEB49'
+          }
+        });
+
+
+        // Tasks
+
+        await strapi.db.query('api::task.task').create({
+          data: {
+            title: 'Create Front end',
+            description: 'Finish Front-end practice',
+            completed: false,
+            category: categoryFront.id
+          }
+        });
+
+        await strapi.db.query('api::task.task').create({
+          data: {
+            title: 'Create Task',
+            description: 'Create Task model',
+            completed: false,
+            category: categoryBack.id
+          }
+        });
+
+        await strapi.db.query('api::task.task').create({
+          data: {
+            title: 'Create Category',
+            description: 'Create Category model and link to Task model',
+            completed: false,
+            category: categoryBack.id
+          }
+        });
+
+        await strapi.db.query('api::task.task').create({
+          data: {
+            title: 'Push practice',
+            description: 'Push changes to the corresponding branch',
+            completed: false,
+            category: categoryOther.id
+          }
+        });
+
+        console.log('Database seeded');
+      } catch (error) {
+        console.error('Error seeding database: ', error);
+      }
+    } else {
+      console.log('Database already seeded');
+    }
+  }
 };
