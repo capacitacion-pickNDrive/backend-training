66bdcdd69688f967dbc19fd8e712c7c9d524c691
emmabeni27 <larisabeni@mail.com>
2025-08-27 22:03:47 -0300

    ultimas modificaciones:

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,16 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://localhost:5173'], // Puerto de tu frontend React
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
